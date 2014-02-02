4cedcbc0327f6b7a7f9c01f7d2ce99c1a5d0384e
Ankan Banerjee <ankan.ban@gmail.com>
2014-02-02 22:38:53 +0530

    random tree generation
    Add test code for
    - random tree generation
    - negaMax search

---
+++ tree.cpp
@@ -0,0 +1,105 @@
+// experiments with alpha-beta search on random tree
+
+#include <stdio.h>
+#include <stdlib.h>    
+#include <math.h>
+
+#define MAX_CHILDREN 64
+#define INF 10000
+
+struct Node
+{
+    float nodeVal;      // value from eval function for leaves, best searched value for interior nodes
+    int bestChild;      // most promising child/branch from this node
+
+    int nChildren;      // no of child nodes
+    Node *children;     // pointer to array containing all child nodes
+
+    Node *parent;       // pointer to parent node
+};
+
+
+int gTotalNodes;
+int gLeafNodes;
+
+void genTree(Node *root, int depth)
+{
+    gTotalNodes++;
+
+    if (depth == 0)
+    {
+        gLeafNodes++;
+        // generate random node val (between 0 and 100) and return
+        root->nodeVal = (rand() % 10000) / 100.0f;
+        return;
+    }
+
+    // allocate memory for random number of children (have at least one children for now - to be changed later!)
+    int nChildren = rand() % MAX_CHILDREN + 1;
+
+    Node *children = (Node *) malloc (nChildren * sizeof(Node));
+
+    for (int i=0; i<nChildren; i++)
+    {
+        children[i].nChildren = 0;
+        children[i].children = NULL;
+        children[i].nodeVal = 0.0f;
+        children[i].parent = root;
+
+        genTree (&children[i], depth - 1);
+    }
+
+    root->nChildren = nChildren;
+    root->children = children;
+}
+
+
+float negaMax(Node *node, int depth, int origDepth)
+{
+    if (depth == 0)
+    {
+        // eval for even depths, -eval for odd depths
+        if (origDepth % 2 == 0)
+            return node->nodeVal;
+        else
+            return -node->nodeVal;
+    }
+
+    // choose the best child
+    float bestScore = -INF;
+    int bestChild = 0;
+
+    for (int i = 0; i < node->nChildren; i++)
+    {
+        float curScore = -negaMax(&node->children[i], depth - 1, origDepth);
+        if (curScore > bestScore)
+        {
+            bestScore = curScore;
+            bestChild = i;
+        }
+    }
+
+    node->nodeVal = bestScore;
+    node->bestChild = bestChild;
+
+    return bestScore;
+}
+
+int main()
+{
+    int depth = 5;
+
+
+    // generate a random tree
+    printf ("generating random tree of depth %d\n", depth);
+    Node root = {0};
+    genTree(&root, depth);
+    printf("random tree generated, total nodes: %d, leaf nodes: %d\n", gTotalNodes, gLeafNodes);
+
+    // search the best move using min-max search
+    printf("searching the tree using min-max\n");
+    float bestVal = negaMax(&root, depth, depth);
+    printf ("best move %d, score: %f\n", root.bestChild, root.nodeVal);
+
+    return 0;
+}
